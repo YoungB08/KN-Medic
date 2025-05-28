c13bb31de61f5017f6f63402c7e5258654c711d3
YoungB08 <knguyen151108@gmail.com>
2025-05-29 01:20:20 +0700

    fix model

--- cronjobs/gpt.php
+++ cronjobs/gpt.php
@@ -1,6 +1,6 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/functions/config.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/models/gpt.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/functions/config.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/models/gpt.php';
 
 $gpt = new GPTDoctor('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');
 
